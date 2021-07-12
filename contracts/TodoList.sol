718d583b4649869643b1bb6d5a4d46ac21ae2f10
Srikanth Govindaraghavan <sri.gr81@gmail.com>
2021-07-12 21:22:22 +0530

    upload js html and node dependencies
    First version

---
+++ contracts/TodoList.sol
@@ -0,0 +1,49 @@
+pragma solidity ^0.5.16;
+
+contract TodoList{
+
+    uint public taskCount = 0;
+
+    struct Task{
+        
+        uint id;
+        string content;
+        bool completed;
+    }
+
+    mapping(uint => Task) public tasks;
+
+    event taskCreated(
+
+        uint id,
+        string content,
+        bool completed
+    );
+
+    event taskCompleted(
+
+        uint id, 
+        bool completed
+    );
+
+    constructor() public{
+
+        createTask("Clear out the karapanpoochi under the mug.");
+    }
+
+    function createTask(string memory _content) public {
+
+        taskCount++;
+        tasks[taskCount] = Task(taskCount, _content, false);
+        emit taskCreated(taskCount, _content, false);
+    }
+
+    function toggleCompleted(uint _id) public {
+
+        Task memory _task = tasks[_id];
+        _task.completed = !_task.completed;
+        tasks[_id] = _task;
+        emit taskCompleted(_id, _task.completed);
+    }
+}
+
